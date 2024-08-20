a52b89f61c9d0f634418191515ea25bc0e46ac58
Joseph Taylor <taylorjoseph3333@gmail.com>
2024-08-20 02:03:24 +0100

    Attempt to make verification script easier to understand

--- verification.py
+++ verification.py
@@ -1,29 +1,30 @@
 
+def case1():
+    x = regs[0]
+    regs[1] -= 1
+    f1()
+    regs[1] = regs[0]
+    regs[0] = x
+    regs[0] -= 1
+    f1()
 
-def f1(depth):
+def case2():
+    regs[0] -= 1
+    regs[1] = n
+    f1()
+
+
+def f1():
     global n
     global regs
     
     if regs[0] > 0:
         #f2(6057)
-        if regs[1] > 0:
-            # Regs[0] was greater than 0.
-            #f3(6070)
-            x = regs[0]
-            regs[1] -= 1
-            f1(depth+1)
-            regs[1] = regs[0]
-            regs[0] = x
-            regs[0] -= 1
-            f1(depth+1)
+        if regs[1] > 0: # case 1
+            case1()
             return
-        else:
-            # Regs[0] was greater than 0.
-
-            # Reset regs[1] to n.
-            regs[0] -= 1
-            regs[1] = n
-            f1(depth+1)
+        else: # case 2
+            case2()
             return
     else:
         #6052
@@ -34,7 +35,7 @@ def f1(depth):
 n = 1
 
 regs = [3, 1]
-f1(0)
+f1()
 print(regs)
 
 """
